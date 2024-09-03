57929788e6a17d016daf1a4d54b4e3ce58a14700
Luiz Felipe <148246609+luizwp@users.noreply.github.com>
2024-09-03 11:18:48 -0300

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm";
+import Image from "next/image";
+
+export default function Home() {
+  return <main>
+    <LoginForm/>
+  </main>
+    
+      
+    
+  ;
+}
---
+++ components/RegisterForm.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function RegisterForm() {
+    return <div className="grid place-items-center h-screen">
+    <div className="shadow-lg p-5 rounded-lg border-t-4 
+    border-green-400">
+        <h1 className="text-xl font-bold my-4">Register</h1>
+
+        <form className="flex flex-col gap-3">
+            <input type="text" placeholder="Full Name" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button className="bg-green-600 text-white 
+            font-bold cursor-pointer px-6  py-2">
+                Register
+            </button>
+
+        <div className="bg-red-500 text-white w-fit
+        text-sm py-1 px-3 rounded-md mt-2">
+            Error message
+        </div>
+
+        <Link className="text-sm mt-3 text-right" href=
+        {'/'}>
+        Already have an account? <span
+        className="underline">Login</span>
+        </Link>
+        </form>
+    </div>
+</div>;
+}
